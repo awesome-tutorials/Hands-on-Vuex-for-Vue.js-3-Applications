ef0158abd0f29076ff1a5f20277d89078bd89954
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-04-08 19:36:47 -0600

    refactor: put all mock data together

--- section-3-reading-tracker-app/tests/unit/helpers.js
+++ section-3-reading-tracker-app/tests/unit/helpers.js
@@ -1,33 +1,38 @@
 import store from "@/store";
+import BOOKS from "@/../backend/books";
 
 const DEFAULT_STATE = JSON.parse(JSON.stringify(store.state));
 export function resetState() {
   store.replaceState(DEFAULT_STATE);
 }
 
-export const NEW_USER = {
-  name: "John Doe",
-  bio: "I like books",
-  email: "test@email.com",
-  password: "123456"
+export const mockData = {
+  BOOKS,
+
+  NEW_USER: {
+    name: "John Doe",
+    bio: "I like books",
+    email: "test@email.com",
+    password: "123456"
+  },
+
+  AUTH_RESPONSE: {
+    accessToken: `abcd.${btoa(JSON.stringify({ sub: 0 }))}.efgh`
+  },
+
+  get SERVER_USER() {
+    return Object.assign(this.NEW_USER, {
+      id: 0,
+      listIds: []
+    });
+  },
+
+  TEST_LIST: {
+    id: 0,
+    userId: 0,
+    name: "Cool Books",
+    description: "Books I think are cool",
+    bookIds: [],
+    books: []
+  }
 };
-
-export const AUTH_RESPONSE = {
-  accessToken: `abcd.${btoa(JSON.stringify({ sub: 0 }))}.efgh`
-};
-
-export const SERVER_USER = Object.assign(NEW_USER, {
-  id: 0,
-  listIds: []
-});
-
-export const TEST_LIST = {
-  id: 0,
-  userId: 0,
-  name: "Cool Books",
-  description: "Books I think are cool",
-  bookIds: [],
-  books: []
-};
-
-export { default as BOOKS } from "@/../backend/books";
